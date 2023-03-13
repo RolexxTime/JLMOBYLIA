f6febcc45e6071b06e7761eef91c69bd68d4c46f
RolexxTime <dindjipierre77@gmail.com>
2023-03-13 15:32:42 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,84 @@
+// AVIS PART
+
+let userTexts = document.getElementsByClassName("user-text"); 
+let userPics = document.getElementsByClassName("user-pic"); 
+
+function showReview(){
+    for(userPic of userPics){
+        userPic.classList.remove("active-pic");
+    }
+    for(userText of userTexts){
+        userText.classList.remove("active-text");
+    }
+    let i = Array.from(userPics).indexOf(event.target);
+
+    userPics[i].classList.add("active-pic");
+    userTexts[i].classList.add("active-text");
+    }
+
+// BLOG PART
+
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const showHideIcons = () =>{
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon =>{
+    icon.addEventListener("click", () =>{
+        let firstImgWidth = firstImg.clientWidth + 14;
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+    });
+});
+
+const dragStart = (e) =>{
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) =>{
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () =>{
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+
+
+// GO TO TOP FUNCTION
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
